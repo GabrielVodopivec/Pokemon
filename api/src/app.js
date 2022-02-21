ac1052ff493b8401ad38b1ad90d02abfbdc5cc90
Gabriel Vodopivec <vodopivecgabriel@gmail.com>
2022-02-21 06:19:00 -0300

    Last update

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,7 @@ const routes = require('./routes/index.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
