d530f0c04f1366f90a81ee745808d5209021e6f2
Gabriel Vodopivec <vodopivecgabriel@gmail.com>
2022-02-21 05:41:49 -0300

    Last update

--- api/src/app.js
+++ api/src/app.js
@@ -17,6 +17,10 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
   next();
 });
 
